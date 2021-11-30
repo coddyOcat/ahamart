b5709a744d28489b08e80fdce31cad3a67b67eab
Đặng Hoài Bão <worldlystorm2111@gmail.com>
2021-11-30 20:19:05 +0700

    hash password

--- server/src/controller/user.js
+++ server/src/controller/user.js
@@ -1,4 +1,24 @@
 const User = require('../model/user')
+const bcrypt = require('bcryptjs')
+
+const hashPassword = (password) => {
+    return new Promise((resolve, reject) => {
+        // Generate a salt at level 12 strength
+        bcrypt.genSalt(12, (err, salt) => {
+        if (err)
+            reject(err);
+  
+        bcrypt.hash(password, salt, (errBc, hash) => {
+            if (errBc)
+                reject(errBc);
+            resolve(hash);
+            });
+        });
+    });
+};
+const verifyPassword = (passwordAttempt, hashedPassword) => {
+    return bcrypt.compare(passwordAttempt, hashedPassword);
+};
 
 exports.insertCustomer = async (req, res) => {
     try {
---
+++ server/src/test.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcryptjs')
+
+const hashPassword = (password) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.genSalt(12, (err, salt) => {
+        if (err)
+            reject(err);
+  
+        bcrypt.hash(password, salt, (errBc, hash) => {
+            if (errBc)
+                reject(errBc);
+            resolve(hash);
+            });
+        });
+    });
+};
+const verifyPassword = (passwordAttempt, hashedPassword) => {
+    return bcrypt.compare(passwordAttempt, hashedPassword);
+};
+const mainz = async () => {
+    const dataPass = "123456abc"
+    const inputPass1 = "123456abc"
+    const inputPass2 = "12345"
+    console.log(await hashPassword(dataPass))
+    console.log(await verifyPassword(inputPass1, await hashPassword(dataPass)))
+    console.log(await verifyPassword(inputPass2, await hashPassword(dataPass)))
+}
+mainz()
